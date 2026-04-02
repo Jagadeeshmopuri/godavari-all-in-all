b75248e87fb85df0ccb6606e0f785033413f4c22
Jagadeeshmopuri <32411712+Jagadeeshmopuri@users.noreply.github.com>
2026-04-02 15:48:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 function bookService(service) {
-  const phone = "91XXXXXXXXXX"; // 👉 put your number
-  const message = `Hello, I need ${service} service in Rajahmundry/Kakinada`;
+  const phone = "91XXXXXXXXXX"; // 👉 replace with your number
+
+  const message = `Hello, I found GodavariHomeFix. I need ${service} service in Rajahmundry/Kakinada`;
 
   const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+
   window.open(url, "_blank");
 }
